df1830d983b4bef4e34cde897e30944c3ab42923
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2016-01-04 13:02:38 -0800

    Fix the post build script

---
+++ ios-plugin/editor/UAPostBuild.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Diagnostics;
+using System.IO;
+
+public class UAPostBuild {
+
+	[PostProcessBuildAttribute(1)]
+	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+	{
+		if (target == BuildTarget.iOS)
+		{
+			
+			UnityEngine.Debug.Log("Running Urban Airship iOS post build script.");
+
+			ProcessStartInfo start = new ProcessStartInfo();
+			start.FileName = "python";
+			start.WorkingDirectory = Application.dataPath + "/Editor/";
+			start.Arguments = String.format("{0} \"{1}\"", "ua_ios_post_build.py", pathToBuiltProject);
+			start.UseShellExecute = false;
+			start.RedirectStandardOutput = true;
+			try {
+				using (Process process = Process.Start(start))
+				{
+					using (StreamReader reader = process.StandardOutput)
+					{
+						UnityEngine.Debug.Log(reader.ReadToEnd());
+					}
+					process.WaitForExit ();
+				}
+			} catch (System.Exception ex) {
+				UnityEngine.Debug.Log(ex.Message);
+			}
+
+			UnityEngine.Debug.Log("Finished Urban Airship iOS post build script.");
+		}
+	}
+		
+}
