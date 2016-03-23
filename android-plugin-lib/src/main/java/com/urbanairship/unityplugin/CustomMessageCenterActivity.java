30e216ccdc4f4f919422a07e2f404713bc8fc244
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2016-03-22 21:51:47 -0700

    [MB-1701] Theme the message center

---
+++ android-plugin-lib/src/main/java/com/urbanairship/unityplugin/CustomMessageCenterActivity.java
@@ -0,0 +1,11 @@
+/*
+ Copyright 2016 Urban Airship and Contributors
+*/
+
+package com.urbanairship.unityplugin;
+
+import com.urbanairship.messagecenter.MessageCenterActivity;
+
+// Copy of the MessageCenterActivity so we can set a different theme
+public class CustomMessageCenterActivity extends MessageCenterActivity {
+}
